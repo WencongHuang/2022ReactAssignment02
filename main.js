2b15662387f0de5735aafde0ac5ce002671ea593
Edmond Huang <43325364+WencongHuang@users.noreply.github.com>
2022-07-12 22:56:24 -0500

    bug fixed for Reload Button
    If user clicks reload button many times during the same interval, weird behavior happens.

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ let resetBtn = document.querySelector(".resetBtn");
 let reloadBtn = document.querySelector(".reloadBtn");
 
 let loadingBar = document.querySelector(".loadingBar");
-let percentage = document.querySelector(".percentage");
+// let percentage = document.querySelector(".percentage");
 let loadingText = document.querySelector(".loadingText");
+
+let timer;
 
 // refresh the page for reset
 resetBtn.addEventListener('click', function() {
@@ -13,12 +15,14 @@ resetBtn.addEventListener('click', function() {
 reloadBtn.addEventListener('click', function() {
   let number = 0;
   
-  let timer = setInterval(() => {
-    percentage.innerHTML = number++;
+  clearInterval(timer);
+
+  timer = setInterval(() => {
+    loadingText.innerHTML = `${number++}%`;
     loadingBar.style.width = String(number) + "%";
     if(number > 100) {
       clearInterval(timer);
-      loadingText.innerHTML = "Finished!"
+      loadingText.innerHTML = "Finished!";
     }
   }, 50);
 });
