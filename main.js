ce7a46d506462b455cfad002f8cb9babb2a0672e
Edmond Huang <43325364+WencongHuang@users.noreply.github.com>
2022-07-14 14:48:24 -0500

    Add arrowDown and arrowUp
    Getting ready for autoComplete search bar.

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+let arrowDown = document.querySelectorAll(".arrowDown");
+let arrowUp = document.querySelectorAll(".arrowUp");
+let nextSection = document.querySelectorAll(".nextSection");
+let previousSection = document.querySelectorAll(".previousSection");
+// add functionality to arrowDown
+for(let i = 0; i < arrowDown.length; i++) {
+  arrowDown[i].addEventListener('click', function() {
+    nextSection[i].click();
+  });
+}
+
+// add functionality to arrowUp
+for(let i = 0; i < arrowUp.length; i++) {
+  arrowUp[i].addEventListener('click', function() {
+    previousSection[i].click();
+  });
+}
+
+// make all the arrow click go smoothly
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+/*
+  Start of Section 1 
+*/
 let resetBtn = document.querySelector(".resetBtn");
 let reloadBtn = document.querySelector(".reloadBtn");
 let pauseBtn = document.querySelector(".pauseBtn");
@@ -60,3 +91,64 @@ pauseBtn.addEventListener('click', function() {
     pauseBtn.innerHTML = "Pause";
   }
 });
+
+/*
+  Start of Section 2 
+*/
+// data from https://www.w3schools.com/howto/howto_js_autocomplete.asp
+let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla",
+"Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan",
+"Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin",
+"Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil",
+"British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia",
+"Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad",
+"Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire",
+"Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica",
+"Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea",
+"Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France",
+"French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana",
+"Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea",
+"Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India",
+"Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan",
+"Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan",
+"Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania",
+"Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali",
+"Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova",
+"Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar",
+"Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia",
+"New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan",
+"Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines",
+"Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda",
+"Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia",
+"Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia",
+"Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain",
+"Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland",
+"Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand",
+"Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan",
+"Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom",
+"United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City",
+"Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+// querySelector or getElementByClassName?
+// let searchBoxInput = document.querySelector(".searchBoxInput");
+// let suggestionList = document.querySelector(".suggestionList");
+
+// searchBoxInput.addEventListener('keydown', function() {
+//   // if(!searchBoxInput.value) {
+//   //   return;
+//   // }
+//   console.log(searchBoxInput.value);
+
+//   // CLEAR THE LIST
+//   for(let i = 0; i < suggestionList.length; i++) {
+//     suggestionList.innerHTML = "";
+//   }
+
+//   for(let i = 0; i < countries.length; i++) {
+//     if(countries[i].substring(0, searchBoxInput.value.length).toUpperCase() == searchBoxInput.value.toUpperCase()) {
+//       let match = document.createElement("li");
+//       match.innerHTML = countries[i];
+//       suggestionList.appendChild(match);
+//     }
+//   }
+// });
